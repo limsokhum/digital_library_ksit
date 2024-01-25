8d33931d7ab373457d48bf73b6aea1c8f5e50479
limsokhum <139779705+limsokhum@users.noreply.github.com>
2024-01-25 18:52:41 +0700

    digital commit

--- admin_dashboard_library/teacher-topbar.php
+++ admin_dashboard_library/teacher-topbar.php
@@ -186,12 +186,13 @@ if($result_select_teacher->num_rows >0){
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
 
                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+
+                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                     Profile
                 </a>
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="logout-teacher.php">
-                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                     Logout
                 </a>
             </div>
